28cacccd5481335ec62dc0b5788a280361599de9
Jungleelucky <67311799+Jungleelucky@users.noreply.github.com>
2021-07-11 14:47:20 +0530

    Add files via upload

--- Rpg.java
+++ Rpg.java
@@ -10,9 +10,10 @@ import java.util.Timer;
 
 public class Rpg
 {
-public void main(){
+public void main() throws InterruptedException {
     
-        
+      boolean merit1 = false;
+      boolean merit2 = false;
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter Your name");
     String name = sc.nextLine();
@@ -82,8 +83,9 @@ public void main(){
             System.out.println("The spider lunges at you when you are doing a three sixty degrees spinning kick your teacher taught you.");
             System.out.println("Your leg finds its mark on the spider's face which was actually quite weak and the spider crumbles into dust.");
             System.out.println("You bow down after performing nicely and notice your audience has crumbled to dust, oh well!");
+            System.out.println("Note: You have earned the ballet merit. This may help you in the future.");
             System.out.println("You then proceed to go right.");
-            
+            merit1 = true;
             
             
             
@@ -143,41 +145,60 @@ System.out.println("A giant worm tears the ground beneath you feet. You rush ahe
 System.out.println("Left or right, what will it be? Remeber the clock is ticking, if you don't make the choice fast enough, the worm will eat you");
 System.out.println("Enter 1 for left and 2 for right!");
 String Crossroad = sc.nextLine();
-long start = System.currentTimeMillis();
-long end = start + 20*1000;
-while (System.currentTimeMillis() < end) {
-    
-if(Crossroad.equals("1")){
-    
-    System.out.println("Test");
-    end = 0;
-    break;
-    
 
 
-}
-else if(Crossroad.equals("2")){
-System.out.println("Test 2");
-end=0;
-break;
 
-}
 
-}
- if(start + 20*1000 == end)
- {
-    System.out.println("You take too much time to decide.");
-    System.out.println("The worm opens its mouth and traps you in it for the rest of your life, which honestly, isn't very long.");
-    System.out.println("The End.");   
+switch(Crossroad){
+case"1":
+    System.out.println("I thought I told you, Right is always the right way. You dash towards the left side hoping to escape the worm");
+    System.out.println("Sadly, you reach a dead end, the worm opens its mouth wide and...");
+    System.out.println("The End.");
     return;
+case"2":
+    System.out.println("The worm follows you and you reach the staircase leading to the second level of the dungeon.");
+    System.out.println("At the same time, the worm roars and charges towards you, giving you a clear view of its soft spot.");
+    System.out.println("If you want to attack the worm's soft spot press 1, press 2 to go to the second level of the dungeon.");
+    String dawg = sc.nextLine(); 
+    if(dawg.equals("1")){
+    System.out.println("You charge towards the worm and slash your sword in its weak spot. You unleash carnage in the chink of its armour.");
+    System.out.println("As the worm dies, it coughs out a gem. You pick up the gem and look at it.");
+    System.out.println("Press 1 to fuse it with your sword");   
+    String fusion = sc.nextLine();
+    if(fusion.equals("1")){
+        System.out.println("Your sword flies out of your hand and glows with a bright light.");
+        System.out.println("You have earned the merit upgrade");
+        merit2 = true;
+        System.out.println("You then enter the second level of the Dungeon.");
+        
+    }
+    else{System.out.println("Why can't you just press the correct buttons? Game Over!");
+    return;}
+}
+  else if(dawg.equals("2")){
+    System.out.println("Fleeing was smart. Better not to risk your life");
+    System.out.println("You have now entered Dungeon level Two!");
+    
+        }  
+        break;
+    default:
+    System.out.println("You presses the wrong button. Code will be terminated.");
+    return;
+}
+System.out.println('\u000C');
+System.out.println("Welcome to Dungeon level 2");
+System.out.println("You found the game hard so far? The game is just about to get harder.");
+System.out.println("You look around and see three directions.");
 
 }
 }
-}
+
+
 
     
     
     
+
 
 
 
